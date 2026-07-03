6913a41cb45cc9a00182650f6ec2681c8190eff0
maxmarusiak <maxmarusiak@codevog.com>
2026-07-03 11:46:21 +0300

    feat: implement variant 1 low grade notification system

--- src/StudentGroupSystem/Menu/MainMenu.cs
+++ src/StudentGroupSystem/Menu/MainMenu.cs
@@ -190,6 +190,9 @@ namespace StudentGroupSystem.Menu
                         _logger.Log("Executing TestExceptionHandling()");
                         _group.TestExceptionHandling();
                         break;
+                    case "61":
+                        TestLowGradeNotification();
+                        break;
 
                     case "0":
                         _logger.Log("Exiting program");
@@ -471,5 +474,12 @@ namespace StudentGroupSystem.Menu
             _fileManager.CleanOldBackups(days);
             Console.WriteLine("Old backups cleaned.");
         }
+        private void TestLowGradeNotification()
+        {
+            var s = new Student("Test Student");
+            s.AddGrade(40);
+            _group.AddStudent(s);
+        }
+
     }
 }
